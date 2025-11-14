3408f0f0e0894d8a3c78ab6cb14021ad7d4c58fb
hmiyamoto2000 <135317049+hmiyamoto2000@users.noreply.github.com>
2025-11-14 10:32:48 +0900

    Fig_2b

---
+++ NMDS_Eukaryota.R
@@ -0,0 +1,40 @@
+
+#install.packages("vegan")
+library(vegan)
+pc = read.csv("Eukaryota_diversity.csv", header= TRUE)
+dim(pc) 
+#make community matrix - extract columns with abundance information
+com = pc[,2:254]
+m_com = as.matrix(com)
+set.seed(123)
+nmds = metaMDS(m_com, distance = "bray")
+nmds
+plot(nmds)
+
+#extract NMDS scores (x and y coordinates) for sites from newer versions of vegan package
+data.scores = as.data.frame(scores(nmds)$sites)
+
+write.csv(data.scores,"NMDS.csv") #一行目にTypeと記載し、分類を記載
+
+values=read.csv("NMDS.csv")
+
+library(pairwiseAdonis)
+values=read.csv("List.csv")
+
+library(pairwiseAdonis)
+p0=pairwise.adonis(pc[,2:997],values$cnd)
+sink('NMDS_pairwiseR_cnd.txt', append = TRUE)　
+print (p0)
+sink()
+
+library(pairwiseAdonis)
+p0=pairwise.adonis(pc[,2:997],values$env)
+sink('NMDS_pairwiseR_envtxt', append = TRUE)　#Comp ThB別
+print (p0)
+sink()
+
+
+
+
+
+
